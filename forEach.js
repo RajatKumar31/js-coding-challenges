8cee83e9bf376b4d0244dd041e6ea447c1b2b4c5
Rajat Kumar <rajatkumar199731@gmail.com>
2025-11-03 16:52:43 +0530

    implement forEach method

--- forEach.js
+++ forEach.js
@@ -1,3 +1,10 @@
 // implement custom Array.prototype.forEach method
 
+Array.prototype.customForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+}
 
+const numbers = [1, 2, 3, 4, 5];
+numbers.customForEach((num) => console.log(num + 1));
