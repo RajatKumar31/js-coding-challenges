dac2b06fde6bb162e0ff26fd21abb4e3de718ce9
Rajat Kumar <rajatkumar199731@gmail.com>
2025-11-10 13:28:39 +0530

    formatting

--- chunkArray.js
+++ chunkArray.js
@@ -5,21 +5,21 @@
  * @param {number} size
  * @return {Array}
  */
-var chunk = function (arr, size) {
-    let res = [];
-    let chunk = [];
-    for (let i = 0; i < arr.length; i++) {
-        chunk.push(arr[i]);
-        if (chunk.length === size) {
-            res.push(chunk);
-            chunk = [];
-        }
-    }
+var chunk = function(arr, size) {
+	let res = [];
+	let chunk = [];
+	for (let i = 0; i < arr.length; i++) {
+		chunk.push(arr[i]);
+		if (chunk.length === size) {
+			res.push(chunk);
+			chunk = [];
+		}
+	}
 
-    if (chunk.length > 0) {
-        res.push(chunk);
-    }
-    return res;
+	if (chunk.length > 0) {
+		res.push(chunk);
+	}
+	return res;
 };
 
 const arr = [1, 2, 3, 4, 5, 6, 7];
