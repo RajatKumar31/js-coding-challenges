9207f0e25eb1d8e21569b9cded929e7c854023bc
Rajat Kumar <rajatkumar199731@gmail.com>
2025-11-10 13:27:39 +0530

    formatting

--- call,apply,bind.js
+++ call,apply,bind.js
@@ -1,47 +1,47 @@
 const car = {
-    color: "Red",
-    company: "BMW"
+	color: "Red",
+	company: "BMW"
 };
 
 function purchaseCar(currency, price) {
-    console.log(`${this.color} ${this.company} car for price ${currency} ${price}`);
+	console.log(`${this.color} ${this.company} car for price ${currency} ${price}`);
 }
 // purchaseCar.call(car, '$', 200);
 // purchaseCar.apply(car, ['$', 900]);
 
-Function.prototype.myCall = function (context = {}, ...args) {
-    console.log("context : ", context);
-    console.log("this : ", this);
-    if (typeof this !== 'function') {
-        throw new Error("not callable");
-    }
-    context.fn = this;
-    context.fn(...args);
+Function.prototype.myCall = function(context = {}, ...args) {
+	console.log("context : ", context);
+	console.log("this : ", this);
+	if (typeof this !== 'function') {
+		throw new Error("not callable");
+	}
+	context.fn = this;
+	context.fn(...args);
 }
 
 purchaseCar.myCall(car, '$', 400);
 
-Function.prototype.myApply = function (context = {}, args = []) {
-    if (typeof this !== 'function') {
-        throw new Error("sdsdf");
-    }
-    if (!Array.isArray(args)) {
-        throw new Error("sdsdf");
-    }
-    context.fn = this;
-    context.fn(...args);
+Function.prototype.myApply = function(context = {}, args = []) {
+	if (typeof this !== 'function') {
+		throw new Error("sdsdf");
+	}
+	if (!Array.isArray(args)) {
+		throw new Error("sdsdf");
+	}
+	context.fn = this;
+	context.fn(...args);
 }
 
 // purchaseCar.myApply(car, ['$', 900]);
 
-Function.prototype.myBind = function (context = {}, ...args) {
-    if (typeof this !== 'function') {
-        throw new Error("sdfsdf");
-    }
-    context.fn = this;
-    return function (...newArgs) {
-        return context.fn(...args, ...newArgs);
-    }
+Function.prototype.myBind = function(context = {}, ...args) {
+	if (typeof this !== 'function') {
+		throw new Error("sdfsdf");
+	}
+	context.fn = this;
+	return function(...newArgs) {
+		return context.fn(...args, ...newArgs);
+	}
 }
 
 // const newFunc = purchaseCar.myBind(car);
