825ef0f8d527480f8427c516452659e8fdeb1e58
Rajat Kumar <rajatkumar199731@gmail.com>
2025-11-12 16:10:05 +0530

    formatting

--- curry.js
+++ curry.js
@@ -1,17 +1,17 @@
 //2632. Curry
 
 function curry(fn) {
-    let nums = [];
-    return function curried(...args) {
-        nums = [...nums, ...args];
-        if (fn.length <= nums.length) {
-            const res = fn(...nums);
-            nums = [];
-            return res;
-        } else {
-            return curried;
-        }
-    };
+	let nums = [];
+	return function curried(...args) {
+		nums = [...nums, ...args];
+		if (fn.length <= nums.length) {
+			const res = fn(...nums);
+			nums = [];
+			return res;
+		} else {
+			return curried;
+		}
+	};
 }
 
 function sum(a, b) { return a + b; }
@@ -19,14 +19,14 @@ const csum = curry(sum);
 console.log(csum(19)(20)); // 3
 
 
-const curry = function (fn) {
-    return function curried(...args) {
-        if (fn.length <= args.length) {
-            return fn(...args);
-        } else {
-            return function (...nextArgs) {
-                return curried(...args, ...nextArgs);
-            };
-        }
-    }
+const curry = function(fn) {
+	return function curried(...args) {
+		if (fn.length <= args.length) {
+			return fn(...args);
+		} else {
+			return function(...nextArgs) {
+				return curried(...args, ...nextArgs);
+			};
+		}
+	}
 }
