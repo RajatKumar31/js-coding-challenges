581e783804517c0bf97d3fabd8e329665328c665
Rajat Kumar <rajatkumar199731@gmail.com>
2025-11-17 17:45:23 +0530

    solve move zeros to left

--- moveZeros.js
+++ moveZeros.js
@@ -3,26 +3,48 @@
  * @returns {void}
  */
 function moveZeros(list) {
-    let j = -1;
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] === 0) {
-            j = i;
-            break;
-        }
-    }
-    if (j === -1) {
-        return;
-    }
-    for (let i = j + 1; i < list.length; i++) {
-        if (list[i] !== 0) {
-            let temp = list[i];
-            list[i] = list[j];
-            list[j] = temp;
-            j++;
-        }
-    }
+	let j = -1;
+	for (let i = 0; i < list.length; i++) {
+		if (list[i] === 0) {
+			j = i;
+			break;
+		}
+	}
+	if (j === -1) {
+		return;
+	}
+	for (let i = j + 1; i < list.length; i++) {
+		if (list[i] !== 0) {
+			let temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+			j++;
+		}
+	}
 }
 
-const list = [1, 0, 0, 2, 3]
-moveZeros(list)
+function moveZerosToLeft(list) {
+	let j = -1;
+	for (let i = list.length - 1; i >= 0; i--) {
+		if (list[i] === 0) {
+			j = i;
+			break;
+		}
+	}
+	if (j === -1) {
+		return list;
+	}
+	for (let i = j - 1; i >= 0; i--) {
+		if (list[i] !== 0) {
+			let temp = list[i]
+			list[i] = list[j];
+			list[j] = temp;
+			j--;
+		}
+	}
+}
+
+const list = [1, 0, 0, 2, 0, 3]
+// moveZeros(list)
+moveZerosToLeft(list)
 console.log(list) // [1,2,3,0,0]
