6ff1ce2ec1f3474d5243ada89752f8567ab26ef8
Rajat Kumar <rajatkumar199731@gmail.com>
2025-11-17 12:43:08 +0530

    solve return occurences in a string

--- returnOccurences.js
+++ returnOccurences.js
@@ -1,6 +1,21 @@
 function returnOccurences(str) {
 	let ans = "";
-	let current, previous;
+	let current = 0, previous = 0;
+	let count = 0;
+	while (current < str.length) {
+		if (str[current] === str[previous]) {
+			count++;
+			previous = current;
+			current++;
+		} else {
+			ans = ans + str[previous] + count;
+			previous = current;
+			count = 0;
+		}
+	}
+	if (count > 0) {
+		ans = ans + str[previous] + count;
+	}
 	return ans;
 }
 
