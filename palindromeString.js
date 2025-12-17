e58c49f51b40f962bc5d8e69c422e2eb5f766ccc
Rajat Kumar <rajatkumar199731@gmail.com>
2025-12-17 17:40:48 +0530

    add another method for palindromeString

--- palindromeString.js
+++ palindromeString.js
@@ -2,16 +2,16 @@ let str = "r#a$ce*^*car*";
 
 // check palindrome string and ignore special characters
 function palindromeString(str) {
-	let i = 0, j=str.length-1;
-	while(i<j) {
-		if(!isAlphaNumeric(str[i])) {
-			i++;	
+	let i = 0, j = str.length - 1;
+	while (i < j) {
+		if (!isAlphaNumeric(str[i])) {
+			i++;
 		}
-		else if(!isAlphaNumeric(str[j])) {
+		else if (!isAlphaNumeric(str[j])) {
 			j--;
 		}
 		else {
-			if(str[i] !== str[j]) {
+			if (str[i] !== str[j]) {
 				return false;
 			}
 			i++;
@@ -22,9 +22,22 @@ function palindromeString(str) {
 }
 console.log(palindromeString(str));
 
+// other method to solve this
+function isPalindrome(str) {
+	str = str.split('');
+	str = str.filter((elem) => isAlphaNumeric(elem)).join('');
+	console.log(str);
+	let str2 = str.split('').reverse().join('');
+	console.log(str2);
+	if (str == str2) {
+		return true;
+	}
+	return false;
+}
+
 function isAlphaNumeric(char) {
-	const asciCode = char.charCodeAt();	
-	if((asciCode >= 97 && asciCode <= 122) || (asciCode >= 65 && asciCode <= 90) || (asciCode>=48 && asciCode <= 57)) {
+	const asciCode = char.charCodeAt();
+	if ((asciCode >= 97 && asciCode <= 122) || (asciCode >= 65 && asciCode <= 90) || (asciCode >= 48 && asciCode <= 57)) {
 		return true;
 	}
 	return false;
