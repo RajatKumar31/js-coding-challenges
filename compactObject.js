cfdcbb931d4a9b59c952627f82062c2fa9d2b5c4
Rajat Kumar <rajatkumar199731@gmail.com>
2025-12-19 12:09:45 +0530

    solve compact object

--- compactObject.js
+++ compactObject.js
@@ -23,3 +23,44 @@ compactObject([null, 0, false, 1]);
 // Input: obj = [null, 0, 5, [0], [false, 16]]
 // Output: [5, [], [16]]
 // Explanation: obj[0], obj[1], obj[3][0], and obj[4][0] were falsy and removed.
+
+/********************************************************************************
+	LEETCODE
+********************************************************************************/
+
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+	if (!Boolean(obj)) {
+		return;
+	}
+	else if (Array.isArray(obj)) {
+		let result = [];
+		for (let elem of obj) {
+			let value = compactObject(elem);
+			if (value) {
+				result.push(value);
+			}
+		}
+		return result;
+	}
+	else if (Object.prototype.toString.call(obj) === "[object Object]") {
+		let result = {};
+		const keys = Object.keys(obj);
+		for (let key of keys) {
+			let value = compactObject(obj[key]);
+			if (value) {
+				result[key] = value;
+			}
+		}
+		return result;
+	}
+	return obj;
+};
+
+// const obj = { "a": null, "b": [false, 1] }
+const obj = [null, 0, 5, [0], [false, 16]]
+const arr = [null, undefined, 1];
+console.log(compactObject(obj));
